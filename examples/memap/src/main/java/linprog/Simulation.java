c4e8729743e9face65024ab5e3a914ca2724fd71
Andreas Thut <andreas.thut@zd-b.de>
2017-12-13 16:40:15 +0100

    Added file that was forgotten in previous commit

--- examples/memap/src/main/java/linprog/Simulation.java
+++ examples/memap/src/main/java/linprog/Simulation.java
@@ -1,11 +1,23 @@
 package linprog;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 import linprog.Simulation;
+import topology.ActorTopology;
 
 public class Simulation {
-
+	
+	private ActorTopology topology;
+	
+	//For the moment, the following 3 parameters need to stay as defined to match data input
+	public static LocalDateTime startTime = LocalDateTime.of(2014,7,1,11,0);
+	public static LocalDateTime endTime = LocalDateTime.of(2014,7,1,12,0);
+	public static Duration timeInterval = Duration.ofMinutes(15);
+	
+	
 	private void run() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
