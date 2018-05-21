7525ae16222e84b148feec82321468f0c64d85d3
Andreas Thut <andreas.thut@zd-b.de>
2018-05-21 23:08:19 +0200

    removed some old code from linprog.

--- examples/memap/src/main/java/ethereum/Topology.java
+++ examples/memap/src/main/java/ethereum/Topology.java
@@ -82,45 +82,6 @@ public class Topology {
 				"0x659cbb0e2411a44db63778987b1e22153c086a95eb6b18bdf89de078917abc63",
 				consumptionProfiles,
 				4));
-//			
-//		String building2Name = "Building2";		
-//		ActorTopology building2 = new ActorTopology(building2Name);		
-//		building2.addActor(building2Name, ActorFactory.createBuilding(building2Name, port++));
-//		building2.addActorAsChild(building2Name + "/Consumption", ActorFactory.createConsumer("Consumption", consumptionProfiles, 1, PORT_UNDEFINED));
-//		building2.addActorAsChild(building2Name + "/GasBoiler", ActorFactory.createGasBoiler("GasBoiler", QDOT_MAX_GASBOILER, EFFICIENCY_GASBOILER, PORT_UNDEFINED));
-//		building2.addActorAsChild(building2Name + "/PV", ActorFactory.createPV("PV", AREA_PV, EFFICIENCY_PV, PORT_UNDEFINED));
-//		building2.addActorAsChild(building2Name + "/HeatPump", ActorFactory.createHeatPump("HeatPump", P_MAX_HEATPUMP, EFFICIENCY_HEATPUMP, PORT_UNDEFINED));
-//		building2.addActorAsChild(building2Name + "/ThermalStorage", ActorFactory.createThermalStorage("ThermalStorage", QDOT_MAX_THERMALSTORAGE_IN, QDOT_MAX_THERMALSTORAGE_OUT, CAPACITY_THERMALSTORAGE, EFFICIENCY_THERMALSTORAGE, PORT_UNDEFINED));
-//				
-//		String building3Name = "Building3";		
-//		ActorTopology building3 = new ActorTopology(building3Name);		
-//		building3.addActor(building3Name, ActorFactory.createBuilding(building3Name, port++));
-//		building3.addActorAsChild(building3Name + "/Consumption", ActorFactory.createConsumer("Consumption", consumptionProfiles, 2, PORT_UNDEFINED));
-//		building3.addActorAsChild(building3Name + "/PV", ActorFactory.createPV("PV", AREA_PV, .2, PORT_UNDEFINED));
-//		building3.addActorAsChild(building3Name + "/Battery", ActorFactory.createBattery("Battery", P_MAX_BATTERY_IN, P_MAX_BATTERY_OUT, CAPACITY_BATTERY, PORT_UNDEFINED));
-//		building3.addActorAsChild(building3Name + "/GasBoiler", ActorFactory.createGasBoiler("GasBoiler", QDOT_MAX_GASBOILER, EFFICIENCY_GASBOILER, port++));
-//
-//		String building4Name = "Building4";		
-//		ActorTopology building4 = new ActorTopology(building4Name);		
-//		building4.addActor(building4Name, ActorFactory.createBuilding(building4Name, port++));
-//		building4.addActorAsChild(building4Name + "/Consumption", ActorFactory.createConsumer("Consumption", consumptionProfiles, 3, port++));
-//		building4.addActorAsChild(building4Name + "/CHP", ActorFactory.createCHP("CHP", QDOT_MAX_CHP, EFFICIENCY_CHP_EL, EFFICIENCY_CHP_H, port++));
-//		building4.addActorAsChild(building4Name + "/ThermalStorage", ActorFactory.createThermalStorage("ThermalStorage", QDOT_MAX_THERMALSTORAGE_IN, QDOT_MAX_THERMALSTORAGE_OUT, CAPACITY_THERMALSTORAGE, EFFICIENCY_THERMALSTORAGE, port++));
-//
-//		String building5Name = "Building5";		
-//		ActorTopology building5 = new ActorTopology(building5Name);		
-//		building5.addActor(building5Name, ActorFactory.createBuilding(building5Name, port++));
-//		building5.addActorAsChild(building5Name + "/Consumption", ActorFactory.createConsumer("Consumption", consumptionProfiles, 4, port++));
-//		building5.addActorAsChild(building5Name + "/CHP", ActorFactory.createCHP("CHP", QDOT_MAX_CHP, EFFICIENCY_CHP_EL, EFFICIENCY_CHP_H, port++));
-//		building5.addActorAsChild(building5Name + "/SolarThermic", ActorFactory.createSolarThermic("SolarThermic", AREA_SOLARTHERMIC, EFFICIENCY_SOLARTHERMIC, port));		
-//		building5.addActorAsChild(building5Name + "/ThermalStorage", ActorFactory.createThermalStorage("ThermalStorage", QDOT_MAX_THERMALSTORAGE_IN, QDOT_MAX_THERMALSTORAGE_OUT, CAPACITY_THERMALSTORAGE, EFFICIENCY_THERMALSTORAGE, port++));		
-//		
-//
-//		top.addSubTopology(simulationName, building3);
-//		top.addSubTopology(simulationName, building2);
-//		top.addSubTopology(simulationName, building1);
-//		top.addSubTopology(simulationName, building4);
-//		top.addSubTopology(simulationName, building5);
 		
 		return top;
 	}
