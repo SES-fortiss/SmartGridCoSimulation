299b19f4c38a7d37bfc47e4fa4b5ea4ed1245281
Adrian.Krueger <Adrian.Krueger@ZD-B-AKR.ZD-B.DE>
2019-03-07 17:36:15 +0100

    Resolved some static/non-static issues with the ConfigInterface

--- projects/memapOpcServer/src/opcMEMAP/ConfigInterface.java
+++ projects/memapOpcServer/src/opcMEMAP/ConfigInterface.java
@@ -1,77 +1,77 @@
 package opcMEMAP;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.net.InetAddress;
-import java.net.DatagramSocket;
 import opcMEMAP.serverConfigurationClassesJSON.MyFolderNode;
 import opcMEMAP.serverConfigurationClassesJSON.MyVariableNode;
 
 public class ConfigInterface {
-	
-	//private String host = "localhost";
-	private static String host;
-	private static int port = 9999;
-	private String uri = "urn:fortiss:opc:sever:memap" + UUID.randomUUID();
-	private String namespace = "sessim";
-	private double minSamplingInterval = 499;
-	private ServerConfigurationImpl serverReference = null;
 
-	private List<MyFolderNode> folderNodeList = new ArrayList<>();
-	private List<MyVariableNode> variableNodeList = new ArrayList<>();
-		
-	/**
-	 * Creates an UDP socket and returns the corresponding IP address. Retrieving
-	 * the IP address of a server is a cumbersome and this might not work on all
-	 * systems. Please refer to this discussion:
-	 * stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
-	 * 
-	 * @return String hostIP-address
-	 */
-	public static String getHost() {
-		try (final DatagramSocket socket = new DatagramSocket()) {
-			socket.connect(InetAddress.getByName("8.8.8.8"), 10002); // 8.8.8.8. can be unreachable, which is ok
-			host = socket.getLocalAddress().getHostAddress();
-		} catch (Exception e) {
-			System.out.println("Invalid IP!");
-		}
-		return host;
-	}
-	
-	public static int getPort() {
-		return port;
-	}
+  // private String host = "localhost";
+  private String host;
+  private int port = 9999;
+  private String uri = "urn:fortiss:opc:sever:memap" + UUID.randomUUID();
+  private String namespace = "sessim";
+  private double minSamplingInterval = 499;
+  private ServerConfigurationImpl serverReference = null;
 
-	public String getUri() {
-		return uri;
-	}
+  private List<MyFolderNode> folderNodeList = new ArrayList<>();
+  private List<MyVariableNode> variableNodeList = new ArrayList<>();
 
-	public String getNamespace() {
-		return namespace;
-	}
+  /**
+   * Creates an UDP socket and returns the corresponding IP address. Retrieving the IP address of a
+   * server is a cumbersome and this might not work on all systems. Please refer to this discussion:
+   * stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
+   * 
+   * @return String hostIP-address
+   */
+  public String getHost() {
+    try (final DatagramSocket socket = new DatagramSocket()) {
+      socket.connect(InetAddress.getByName("8.8.8.8"), 10002); // 8.8.8.8. can be unreachable, which
+                                                               // is ok
+      host = socket.getLocalAddress().getHostAddress();
+    } catch (Exception e) {
+      System.out.println("Invalid IP!");
+    }
+    return host;
+  }
 
-	public List<MyFolderNode> getFolderNodeList() {
-		return folderNodeList;
-	}
+  public int getPort() {
+    return port;
+  }
 
-	public List<MyVariableNode> getVariableNodeList() {
-		return variableNodeList;
-	}
+  public String getUri() {
+    return uri;
+  }
 
-	public double getMinSamplingInterval() {
-		return minSamplingInterval;
-	}
+  public String getNamespace() {
+    return namespace;
+  }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+  public List<MyFolderNode> getFolderNodeList() {
+    return folderNodeList;
+  }
 
-	public ServerConfigurationImpl getServerReference() {
-		return serverReference;
-	}
+  public List<MyVariableNode> getVariableNodeList() {
+    return variableNodeList;
+  }
 
-	public void setServerReference(ServerConfigurationImpl serverReference) {
-		this.serverReference = serverReference;
-	}
+  public double getMinSamplingInterval() {
+    return minSamplingInterval;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+  public ServerConfigurationImpl getServerReference() {
+    return serverReference;
+  }
+
+  public void setServerReference(ServerConfigurationImpl serverReference) {
+    this.serverReference = serverReference;
+  }
 }
