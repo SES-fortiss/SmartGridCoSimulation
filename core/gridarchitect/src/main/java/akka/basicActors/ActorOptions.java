ba14478402896da84195a095518105e787594392
Adrian.Krueger <Adrian.Krueger@ZD-B-AKR.ZD-B.DE>
2019-03-22 13:56:58 +0100

    Some further improvements

--- core/gridarchitect/src/main/java/akka/basicActors/ActorOptions.java
+++ core/gridarchitect/src/main/java/akka/basicActors/ActorOptions.java
@@ -1,23 +1,20 @@
 /*
- * Copyright (c) 2011-2015, fortiss GmbH.
- * Licensed under the Apache License, Version 2.0.
+ * Copyright (c) 2011-2015, fortiss GmbH. Licensed under the Apache License, Version 2.0.
  *
- * Use, modification and distribution are subject to the terms specified
- * in the accompanying license file LICENSE.txt located at the root directory
- * of this software distribution.
+ * Use, modification and distribution are subject to the terms specified in the accompanying license
+ * file LICENSE.txt located at the root directory of this software distribution.
  */
 
 package akka.basicActors;
 
 import java.util.HashSet;
-
 import behavior.BehaviorModel;
 
 /**
  * This class is the basic Builder
  * 
  * @param loggingMode - DEBUG, PRODUCTION, UNIT_TEST, MINIMAL
- * @param childrenNameList - List with paths to children 
+ * @param childrenNameList - List with paths to children
  * @param directConnectionsPathList - List with paths to direct connections
  * @param behaviorModel - Derived classes of the behaviorParameter class
  * @param savingInterface - Saving Interface (NoSave(), SaveCSV, ...
@@ -27,51 +24,47 @@ import behavior.BehaviorModel;
  */
 
 public class ActorOptions {
-	
-    public LoggingMode loggingMode;
-	public HashSet<String> childrenNameList;
-	public HashSet<String> childrenPathList;
-    public HashSet<String> directConnectionsPathList;   
 
-    // behavior represents all kinds of behavior
-    public BehaviorModel behaviorModel;
-    public boolean hasAlreadyBeenSpawned = false;    
+  public LoggingMode loggingMode;
+  public HashSet<String> childrenNameList;
+  public HashSet<String> childrenPathList;
+  public HashSet<String> directConnectionsPathList;
 
-    @Override
-    public String toString() {
-        return "BasicBuilder{" +
-                "loggingMode=" + loggingMode +
-                ", childrenNameList=" + childrenNameList +
-                ", childrenPathList=" + childrenPathList +
-                ", directConnectionsList=" + directConnectionsPathList +
-                ", behaviorModel=" + behaviorModel +
-                '}';
-    }
+  // behavior represents all kinds of behavior
+  public BehaviorModel behaviorModel;
+  public boolean hasAlreadyBeenSpawned = false;
 
-    /**
-     * The preferred BasicActor constructor
-     * 
-     * @param loggingMode - DEBUG, PRODUCTION, UNIT_TEST, MINIMAL
-     * @param childrenNameList - List with names to children 
-     * @param childrenPathList - List with fullActorPaths of the children 
-     * @param directConnectionsPathList - List with paths to direct connections
-     * @param behaviorModel - Derived classes of the behaviorParameter class
-     * 
-     * @author bytschkow
-     */    
-    
-    public ActorOptions(LoggingMode loggingMode, HashSet<String> childrenNameList, HashSet<String> childrenPathList,
-    					HashSet<String> directConnectionsPathList,
-                        BehaviorModel behaviorModel) {
-        this.loggingMode = loggingMode;
-        this.childrenNameList = childrenNameList;
-        this.childrenPathList = childrenPathList;
-        this.directConnectionsPathList = directConnectionsPathList;
-        this.behaviorModel = behaviorModel;
-    }
+  @Override
+  public String toString() {
+    return "BasicBuilder{" + "loggingMode=" + loggingMode + ", childrenNameList=" + childrenNameList
+        + ", childrenPathList=" + childrenPathList + ", directConnectionsList="
+        + directConnectionsPathList + ", behaviorModel=" + behaviorModel + '}';
+  }
 
-    /**
-     * Empty Constructor needed for JSON serialization, currently used for visualization
-     */
-    public ActorOptions(){}    
+  /**
+   * The preferred BasicActor constructor
+   * 
+   * @param loggingMode - DEBUG, PRODUCTION, UNIT_TEST, MINIMAL
+   * @param childrenNameList - List with names to children
+   * @param childrenPathList - List with fullActorPaths of the children
+   * @param directConnectionsPathList - List with paths to direct connections
+   * @param device - Derived classes of the behaviorParameter class
+   * 
+   * @author bytschkow
+   */
+
+  public ActorOptions(LoggingMode loggingMode, HashSet<String> childrenNameList,
+      HashSet<String> childrenPathList, HashSet<String> directConnectionsPathList,
+      BehaviorModel device) {
+    this.loggingMode = loggingMode;
+    this.childrenNameList = childrenNameList;
+    this.childrenPathList = childrenPathList;
+    this.directConnectionsPathList = directConnectionsPathList;
+    this.behaviorModel = device;
+  }
+
+  /**
+   * Empty Constructor needed for JSON serialization, currently used for visualization
+   */
+  public ActorOptions() {}
 }
---
+++ projects/memapLinProgDenisThesis/src/main/java/linprogMPC/controller/BuildingController.java
@@ -0,0 +1,15 @@
+package linprogMPC.controller;
+
+import java.util.Set;
+
+import linprogMPC.components.prototypes.Device;
+
+public interface BuildingController {
+    public String getName();
+
+    public boolean hasLDHeaeting();
+
+    public int getHeatTransportLength();
+
+    public Set<? extends Device> getDevices();
+}
