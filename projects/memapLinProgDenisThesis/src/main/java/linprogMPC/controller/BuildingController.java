0d5c6e74a4629cdce2355184779507f9fc5cd6df
Adrian.Krueger <Adrian.Krueger@ZD-B-AKR.ZD-B.DE>
2019-04-16 13:44:19 +0200

    Changed CSVExampleController to CSVBuildingController. Added Javadoc.

--- projects/memapLinProgDenisThesis/src/main/java/linprogMPC/controller/BuildingController.java
+++ projects/memapLinProgDenisThesis/src/main/java/linprogMPC/controller/BuildingController.java
@@ -4,6 +4,14 @@ import java.util.Set;
 
 import linprogMPC.components.prototypes.Device;
 
+/**
+ * The BuildingController Interface provides all methods needed for dealing with
+ * buildings. As for now, CSV files are used for consumption information, but
+ * later this interface might be implemented by a OPCua client.
+ * 
+ * @see CSVBuildingController
+ * @author Adrian.Krueger
+ */
 public interface BuildingController {
     public String getName();
 
