2ab4856a67e826f4ccde697f36d6e70ca9d26bfa
Adrian.Krueger <Adrian.Krueger@ZD-B-AKR.ZD-B.DE>
2019-04-18 10:55:58 +0200

    Commented out OPC UA Reader from Building.class to make program runnable again

--- projects/memapLinProgDenisThesis/src/main/java/linprogMPC/ThesisSimulation.java
+++ projects/memapLinProgDenisThesis/src/main/java/linprogMPC/ThesisSimulation.java
@@ -57,7 +57,7 @@ public class ThesisSimulation {
 
 	// **************MEMAP OFF *******************
 
-		// int[] mpcTimeSteps = { 4 };
+	// int[] mpcTimeSteps = { 4 };
 	// for (int i = 0; i < mpcTimeSteps.length; i++) {
 	// topology = ThesisTopologySimple.createTopology(mpcTimeSteps[i], false);
 	// ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(topology);
@@ -72,6 +72,7 @@ public class ThesisSimulation {
 //	    ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(topology);
 //	    SimulationStarter.startSimulation(actorSystem, 0, ThesisTopologySimple.NR_OF_ITERATIONS);
 //	}
+    }
 
     private static String showHelp() {
 	ConfigInterface jsonInterface = new ConfigInterface();
