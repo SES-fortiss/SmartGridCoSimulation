b60382eee9d71c2f286f32b11c9d9e90f1c70acc
Timo <timo.freiesleben@zd-b.de>
2019-07-16 14:35:47 +0200

    Jetty Server displays all current building data. Hope this time without Git problems.

---
+++ projects/memapLinProgDenisThesis/src/main/java/linprogMPC/JettyStart.java
@@ -0,0 +1,135 @@
+package linprogMPC;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+
+import akka.actor.ActorSystem;
+import linprogMPC.controller.BuildingController;
+import linprogMPC.controller.OpcUaBuildingController;
+import linprogMPC.controller.TopologyController;
+import opcMEMAP.ConfigInterface;
+import simulation.SimulationStarter;
+import topology.ActorTopology;
+
+public class JettyStart {
+
+//	public ActorTopology topology;
+	public TopologyController topologyController;
+
+	public void main(String arg, JsonArray endpoint) {
+		// show help by default
+
+		switch (arg) {
+		case "help":
+		case "h":
+			System.out.println(showHelp());
+			break;
+		// run the simulation
+		case "start":
+		case "s":
+			new JettyStart().run(endpoint);
+			break;
+		// run the simulation in a loop
+		case "loop":
+		case "l":
+			boolean loop = true;
+			while (loop) {
+				new JettyStart().run(endpoint);
+				break;
+			}
+		default:
+			System.out.println(showHelp());
+			break;
+		}
+	}
+	
+	public TopologyController getTopology() {
+		return topologyController;
+	}
+	
+	public void run(JsonArray endpointValues) {
+		topologyController = new TopologyController("MEMAP", true, 1, 96, 7, 0, false, 9999);
+
+		
+		
+		//So far, Config Nodes are hard coded. For future versions the Node Json has to be provided by the local EMS
+		JsonObject jsonNodes1=null;
+		try {
+			FileReader nodes1 = new FileReader("src/main/java/linprogMPC/controller/FortissBuilding1Nodes.json");
+			jsonNodes1 = (JsonObject) Jsoner.deserialize(nodes1);
+		} catch (FileNotFoundException | JsonException e1) {
+			System.err.println("WARNING: Failed to read JSON config files. Building has not been initalised.");
+			e1.printStackTrace();
+		}
+
+		//Iterating through all the endpoint Jsons inputed in the user interface
+		//generates a building controller for every jsonEndpoint,jsonNodes tuple
+		//Buildings get attached to the topology
+		for (int i=0;i<endpointValues.size();i++) {
+		try {
+				
+					JsonObject jsonEndpoint = (JsonObject) endpointValues.get(i);
+					BuildingController sampleBuilding = new OpcUaBuildingController(jsonEndpoint, jsonNodes1);
+					topologyController.attach(sampleBuilding);
+				
+			
+		} catch (IllegalStateException e2) {
+			System.err.println("WARNING: Failed to create Client. Building has not been initialised");
+			e2.printStackTrace();
+		}
+		}
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // Wait so that we get initial values for all devices
+		
+		
+		
+		//Here, the topology Controller gets started. However, if activated, the thread runs through and thereby blocks our Jetty Server.
+		
+//		topologyController.startSimulation();
+
+		// To test the optimizer with csv files, maybe try this:
+//	topology = FiveBuildingExample.exampleTopology(true);
+//	ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(topology);
+//	SimulationStarter.startSimulation(actorSystem, 0, ThesisTopologySimple.NR_OF_ITERATIONS);
+
+		// **************MEMAP OFF *******************
+
+		// int[] mpcTimeSteps = { 4 };
+		// for (int i = 0; i < mpcTimeSteps.length; i++) {
+		// topology = ThesisTopologySimple.createTopology(mpcTimeSteps[i], false);
+		// ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(topology);
+		// SimulationStarter.startSimulation(actorSystem, 0,
+		// ThesisTopologySimple.NR_OF_ITERATIONS);
+		// }
+
+		// **************MEMAP ON ********************
+//	int[] mpcTimeSteps2 = { 1, 4, 12, 24, 36, 48, 60, 72 };
+//	for (int i = 0; i < mpcTimeSteps2.length; i++) {
+//	    topology = ThesisTopologySimple.createTopology(mpcTimeSteps2[i], true);
+//	    ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(topology);
+//	    SimulationStarter.startSimulation(actorSystem, 0, ThesisTopologySimple.NR_OF_ITERATIONS);
+//	}
+	}
+
+	private static String showHelp() {
+		ConfigInterface jsonInterface = new ConfigInterface();
+		String server = jsonInterface.getHost();
+		int port = jsonInterface.getPort();
+		String help = "Hi there, this is MEMAP.\n" + "To reach the OPC-UA server on this machine, please use opc.tcp://"
+				+ server + ":" + port + "/sessim\n" + "\n"
+				+ "Use these commands to run the simulation (i.e. use MemapServer.jar [command]: \n"
+				+ "    start : runs the simulation once \n" + "    loop  : runs the simulation in an indefinite loop \n"
+				+ "    help  : show the help screen \n";
+		return help;
+	}
+}
---
+++ projects/memapLinProgDenisThesis/src/main/java/linprogMPC/websocket/EchoSocket.java
@@ -0,0 +1,152 @@
+package linprogMPC.websocket;
+
+import java.util.Iterator;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+
+import linprogMPC.JettyStart;
+import linprogMPC.components.ClientConsumer;
+import linprogMPC.components.ClientCoupler;
+import linprogMPC.components.ClientProducer;
+import linprogMPC.components.ClientStorage;
+import linprogMPC.components.ClientVolatileProducer;
+import linprogMPC.components.prototypes.Device;
+import linprogMPC.controller.BuildingController;
+import linprogMPC.controller.TopologyController;;
+
+
+
+public class EchoSocket extends WebSocketAdapter
+{
+    private static final Logger LOG = Log.getLogger(EchoSocket.class);
+
+    public void onWebSocketClose(int statusCode, String reason)
+    {
+        super.onWebSocketClose(statusCode,reason);
+        LOG.info("WebSocket Close: {} - {}",statusCode,reason);
+    }
+
+    public void onWebSocketConnect(Session session)
+    {
+        super.onWebSocketConnect(session);
+        LOG.info("WebSocket Connect: {}",session);
+//        getRemote().sendStringByFuture("You are now connected to " + this.getClass().getName());
+    }
+
+    public void onWebSocketError(Throwable cause)
+    {
+        LOG.warn("WebSocket Error",cause);
+    }
+
+    public void onWebSocketText(String message)
+    {
+    	//Websocket can only surpass Strings. Hence, message is of type String.
+    	//In our case the message consists of a String representing the surpassed JsonArray Therefore, we need to split it first.
+
+    	String[] messageArray=message.split("\\},"); 	
+        for (int i=0; i < messageArray.length -1;i++) {
+        	messageArray[i]=messageArray[i]+"}";	
+        }
+    	
+
+    	
+    	//Transfer Strings to Jsons. 
+        //messageJsonArray is of JsonArray format
+        JsonArray messageJsonArray = new JsonArray();
+        for (int i = 0; i < messageArray.length; i++) {
+			try {
+				JsonObject messageJson = (JsonObject) Jsoner.deserialize(messageArray[i]);
+				messageJsonArray.add(messageJson);
+			} catch (JsonException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+    	
+        // Connection to selected endpoints. Moreover, Simulation can get started.(See JettyStart in linprogMPC)
+        JettyStart js=new JettyStart();
+        js.run(messageJsonArray);
+
+
+
+        // Regular Update (every 10 sec) of current Building data.
+        //Every Building is iterated through
+        //Every Device in every Building is iterated through
+        //Key Values of each device are displayed
+    	  		Runnable helloRunnable = new Runnable() {
+  		    public void run() {
+  		    	Iterator<BuildingController> iterator=js.getTopology().managedBuildings.iterator();
+		    		getRemote().sendStringByFuture("empty");
+		    		
+  		    	while (iterator.hasNext()) {
+	  				BuildingController build=iterator.next();
+	  				String name=build.getName();
+	  				getRemote().sendStringByFuture(" ");
+	  				getRemote().sendStringByFuture("Name of Building: " + name);
+	  				getRemote().sendStringByFuture(" Connection Status: Is Connected");
+	  				
+	  				Iterator<? extends Device> devices= build.getDevices().iterator();
+
+	  				while (devices.hasNext()) {
+	  				Device device=devices.next();
+	  				
+	  				if (device instanceof ClientConsumer) {
+	  				    ClientConsumer consumer = (ClientConsumer) device;
+	  				    getRemote().sendStringByFuture("    Consumer");
+	  				    String output="        Electricity Profile: "+consumer.electricityProfile.get(0) + " Heat Profile: "+ consumer.heatProfile.get(0);
+	  				    getRemote().sendStringByFuture(output);
+	  				}
+	  				if (device instanceof ClientCoupler) {
+	  				    ClientCoupler coupler = (ClientCoupler) device;
+	  				    getRemote().sendStringByFuture("    Coupler");
+	  				    String output="        Efficiency Elec: "+coupler.efficiencyElec+" Efficiency Heat: "+ coupler.efficiencyHeat;
+	  				    getRemote().sendStringByFuture(output);
+	  				}
+	  				if (device instanceof ClientProducer) {
+	  				    ClientProducer producer = (ClientProducer) device;
+	  				    getRemote().sendStringByFuture("    Producer");
+	  				    String output="        Costs: "+producer.costs+" Efficiency: "+producer.efficiency;
+	  				    getRemote().sendStringByFuture(output);
+	  				}
+	  				if (device instanceof ClientStorage) {
+	  				    ClientStorage storage = (ClientStorage) device;
+	  				    getRemote().sendStringByFuture("    Storage");
+	  				    String output="        State of Charge: "+ storage.myStateOfCharge+" Capacity " + storage.capacity;
+	  				    getRemote().sendStringByFuture(output);
+	  				}
+	  				if (device instanceof ClientVolatileProducer) {
+	  				    ClientVolatileProducer volatileProducer = (ClientVolatileProducer) device;
+	  				    getRemote().sendStringByFuture("VolatileProducer");
+	  				    String output="        Efficiency: " + volatileProducer.efficiency+" Installed Power: " + volatileProducer.installedPower;
+	  				    getRemote().sendStringByFuture(output);
+	  				}}
+	  				
+//	  				elect=Math.round(100.0 * elect) / 100.0;
+  		    }   		
+  		    
+  		    }
+  		};
+  		
+  		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+  		executor.scheduleAtFixedRate(helloRunnable, 1, 10, TimeUnit.SECONDS);
+
+            LOG.info("Sending: Successful");
+
+    }
+
+    @Override
+    public void onWebSocketBinary(byte[] arg0, int arg1, int arg2)
+    {
+        /* ignore */
+    }
+}
