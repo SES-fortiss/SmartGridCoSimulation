c68a1da4d7c9e9f437a977f1679e4477087bfa38
highvight <highvight@tutamail.com>
2019-08-26 11:02:47 +0200

    Delete package info of linprogMPC.controller

--- projects/memapLinProgDenisThesis/src/main/java/linprogMPC/controller/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The controllers in this package shall help to implement the MVC-pattern. In
- * contains mostly interfaces to control the model like a buildingController and
- * a topologyController.
- * 
- * @author Adrian.Krueger
- *
- */
-package linprogMPC.controller;
