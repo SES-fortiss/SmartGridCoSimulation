2eba2894409b317d8b6830ab67d5862e008d66b6
highvight <highvight@tutamail.com>
2019-08-26 13:29:58 +0200

    Refactores main method and examples

--- projects/memapLinProgDenisThesis/src/main/java/linprogMPC/ThesisSimulation.java
+++ projects/memapLinProgDenisThesis/src/main/java/linprogMPC/ThesisSimulation.java
@@ -1,15 +1,7 @@
 package linprogMPC;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-import com.github.cliftonlabs.json_simple.JsonException;
-import com.github.cliftonlabs.json_simple.JsonObject;
-import com.github.cliftonlabs.json_simple.Jsoner;
-
-import linprogMPC.controller.BuildingController;
-import linprogMPC.controller.OpcUaBuildingController;
 import linprogMPC.controller.TopologyController;
+import linprogMPC.examples.ExampleLoader;
 import linprogMPC.websocket.JettyWebsocket;
 import opcMEMAP.ConfigInterface;
 
@@ -21,8 +13,8 @@ import opcMEMAP.ConfigInterface;
  * help: displays help menu start: initializes connection to the two fortiss
  * servers and starts simulation with these two example "buildings". loop:
  * starts simulation in loop jetty: starts the jetty server, available on port
- * 8080. There, one can input Connection data by her or himself and also start
- * the simulation.
+ * 8080. csv: start a simulation with csv files. There, one can input Connection
+ * data by her or himself and also start the simulation.
  * 
  * @author freiesleben & krueger
  * 
@@ -31,31 +23,46 @@ import opcMEMAP.ConfigInterface;
 public class ThesisSimulation {
 
     public static void main(String[] args) {
-	// show help by default
 	String arg = "help";
-	// if argument is passed, use argument instead
-	if (args.length == 1) {
+
+	if (args.length != 0) {
 	    arg = args[0];
 	}
 
+	argParser(arg);
+
+    }
+
+    private static void run(TopologyController topologyController) {
+	topologyController.startSimulation();
+    }
+
+    private static void argParser(String arg) {
 	switch (arg) {
 	case "help":
 	case "h":
 	    System.out.println(showHelp());
 	    break;
+
 	// run the simulation
 	case "start":
 	case "s":
-	    new ThesisSimulation().run();
+	    run(ExampleLoader.OpcUaExample);
 	    break;
+
 	// run the simulation in a loop
 	case "loop":
 	case "l":
-	    boolean loop = true;
-	    while (loop) {
-		new ThesisSimulation().run();
+	    while (true) {
+		run(ExampleLoader.OpcUaExample);
 	    }
+
+	    // run csv example once
+	case "csv":
+	    run(ExampleLoader.CsvExample);
 	    break;
+
+	// starts jetty Server
 	case "jetty":
 	case "j":
 	    String[] arbitraryValue = null;
@@ -65,76 +72,7 @@ public class ThesisSimulation {
 	    System.out.println(showHelp());
 	    break;
 	}
-    }
 
-    private void run() {
-	TopologyController topologyController = new TopologyController("MEMAP", true, 5, 96, 7, 0, false, 4880);
-
-	try {
-	    BufferedReader endpoint1 = new BufferedReader(new InputStreamReader(
-		    this.getClass().getClassLoader().getResourceAsStream("opcconfig/FortissBuilding1Endpoint.json")));
-	    BufferedReader nodes1 = new BufferedReader(new InputStreamReader(
-		    this.getClass().getClassLoader().getResourceAsStream("opcconfig/FortissBuilding1Nodes.json")));
-	    JsonObject jsonEndpoint1 = (JsonObject) Jsoner.deserialize(endpoint1);
-	    JsonObject jsonNodes1 = (JsonObject) Jsoner.deserialize(nodes1);
-	    BuildingController sampleBuilding1 = new OpcUaBuildingController(jsonEndpoint1, jsonNodes1);
-	    topologyController.attach(sampleBuilding1);
-	} catch (JsonException e1) {
-	    System.err.println("WARNING: Failed to read JSON config files. Building has not been initalised.");
-	    e1.printStackTrace();
-	} catch (IllegalStateException e2) {
-	    System.err.println("WARNING: Failed to create Client. Building has not been initialised");
-	    e2.printStackTrace();
-	}
-
-	try {
-	    BufferedReader endpoint2 = new BufferedReader(new InputStreamReader(
-		    this.getClass().getClassLoader().getResourceAsStream("opcconfig/FortissBuilding2Endpoint.json")));
-	    BufferedReader nodes2 = new BufferedReader(new InputStreamReader(
-		    this.getClass().getClassLoader().getResourceAsStream("opcconfig/FortissBuilding2Nodes.json")));
-	    JsonObject jsonEndpoint2 = (JsonObject) Jsoner.deserialize(endpoint2);
-	    JsonObject jsonNodes2 = (JsonObject) Jsoner.deserialize(nodes2);
-	    BuildingController sampleBuilding2 = new OpcUaBuildingController(jsonEndpoint2, jsonNodes2);
-	    topologyController.attach(sampleBuilding2);
-	} catch (JsonException e1) {
-	    System.err.println("WARNING: Failed to read JSON config files. Building has not been initalised.");
-	    e1.printStackTrace();
-	} catch (IllegalStateException e2) {
-	    System.err.println("WARNING: Failed to create Client. Building has not been initialised");
-	    e2.printStackTrace();
-	}
-
-	try {
-	    Thread.sleep(5000);
-	} catch (InterruptedException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} // Wait so that we get initial values for all devices
-
-	topologyController.startSimulation();
-
-	// To test the optimizer with csv files, maybe try this:
-//	topology = FiveBuildingExample.exampleTopology(true);
-//	ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(topology);
-//	SimulationStarter.startSimulation(actorSystem, 0, ThesisTopologySimple.NR_OF_ITERATIONS);
-
-	// **************MEMAP OFF *******************
-
-	// int[] mpcTimeSteps = { 4 };
-	// for (int i = 0; i < mpcTimeSteps.length; i++) {
-	// topology = ThesisTopologySimple.createTopology(mpcTimeSteps[i], false);
-	// ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(topology);
-	// SimulationStarter.startSimulation(actorSystem, 0,
-	// ThesisTopologySimple.NR_OF_ITERATIONS);
-	// }
-
-	// **************MEMAP ON ********************
-//	int[] mpcTimeSteps2 = { 1, 4, 12, 24, 36, 48, 60, 72 };
-//	for (int i = 0; i < mpcTimeSteps2.length; i++) {
-//	    topology = ThesisTopologySimple.createTopology(mpcTimeSteps2[i], true);
-//	    ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(topology);
-//	    SimulationStarter.startSimulation(actorSystem, 0, ThesisTopologySimple.NR_OF_ITERATIONS);
-//	}
     }
 
     private static String showHelp() {
@@ -146,7 +84,9 @@ public class ThesisSimulation {
 		+ "Use these commands to run the simulation (i.e. use MemapServer.jar [command]: \n"
 		+ "    start : runs the simulation once \n" + "    loop  : runs the simulation in an indefinite loop \n"
 		+ "    jetty  : start jetty webserver. (Available under 8080) \n"
+		+ "    csv  : start a simulation reading csv files instead of ems servers \n"
 		+ "    help  : show the help screen \n";
 	return help;
     }
+
 }
--- projects/memapLinProgDenisThesis/src/main/java/linprogMPC/controller/TopologyController.java
+++ projects/memapLinProgDenisThesis/src/main/java/linprogMPC/controller/TopologyController.java
@@ -26,75 +26,76 @@ import topology.ActorTopology;
  */
 public class TopologyController extends ThesisTopologySimple {
 
-	public int nrStepsMPC;
-	public boolean memapOn;
-	public int portUndefined;
-	public String name;
-	public int timeStepsPerDay;
-	public int nrDays;
-	public int predUncertainty;
-	public boolean hasLDHeating;
+    public int nrStepsMPC;
+    public boolean memapOn;
+    public int portUndefined;
+    public String name;
+    public int timeStepsPerDay;
+    public int nrDays;
+    public int predUncertainty;
+    public boolean hasLDHeating;
 
-	public Set<BuildingController> managedBuildings = new HashSet<>();
+    public Set<BuildingController> managedBuildings = new HashSet<>();
 
-	public ActorTopology top;
+    public ActorTopology top;
 
-	public TopologyController(String name, boolean memapOn, int nrStepsMPC, int timeStepsPerDay, int nrDays,
-			int predUncertainty, boolean hasLDHeating, int portUndefined) {
-		this.name = name;
-		this.nrStepsMPC = nrStepsMPC;
-		this.memapOn = memapOn;
-		this.portUndefined = portUndefined;
-		this.timeStepsPerDay = timeStepsPerDay;
-		this.nrDays = nrDays;
-		this.predUncertainty = predUncertainty;
-		this.hasLDHeating = hasLDHeating;
+    public TopologyController(String name, boolean memapOn, int nrStepsMPC, int timeStepsPerDay, int nrDays,
+	    int predUncertainty, boolean hasLDHeating, int portUndefined) {
+	this.name = name;
+	this.nrStepsMPC = nrStepsMPC;
+	this.memapOn = memapOn;
+	this.portUndefined = portUndefined;
+	this.timeStepsPerDay = timeStepsPerDay;
+	this.nrDays = nrDays;
+	this.predUncertainty = predUncertainty;
+	this.hasLDHeating = hasLDHeating;
 
-		ThesisTopologySimple.setStepsMPC(nrStepsMPC);
-		ThesisTopologySimple.setTimeStepsPerDay(timeStepsPerDay);
-		ThesisTopologySimple.setPredUncertainty(predUncertainty);
-		ThesisTopologySimple.setMemapLDHeating(hasLDHeating);
-		ThesisTopologySimple.setMemapOn(memapOn);
-		ThesisTopologySimple.setNrOfDays(nrDays);
-		ThesisTopologySimple.calcNrIterations();
-		ThesisTopologySimple.calcNrSteps();
+	ThesisTopologySimple.setStepsMPC(nrStepsMPC);
+	ThesisTopologySimple.setTimeStepsPerDay(timeStepsPerDay);
+	ThesisTopologySimple.setPredUncertainty(predUncertainty);
+	ThesisTopologySimple.setMemapLDHeating(hasLDHeating);
+	ThesisTopologySimple.setMemapOn(memapOn);
+	ThesisTopologySimple.setNrOfDays(nrDays);
+	ThesisTopologySimple.calcNrIterations();
+	ThesisTopologySimple.calcNrSteps();
+    }
+
+    public void attach(BuildingController buildingController) {
+	managedBuildings.add(buildingController);
+    }
+
+    public void startSimulation() {
+	createTopology();
+	ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(this.top);
+	SimulationStarter.startSimulation(actorSystem, 0, ThesisTopologySimple.NR_OF_ITERATIONS);
+    }
+
+    public void endSimulation() {
+	SimulationStarter.stopSimulation();
+    }
+
+    private void createTopology() {
+	// Creating Actor Topology
+	this.top = new ActorTopology(this.name);
+	LinProgBehavior linProg = new LinProgBehavior();
+	top.addActor(this.name, ActorFactory.createDevice(linProg));
+
+	for (BuildingController managedBuilding : managedBuildings) {
+	    String buildingName = managedBuilding.getName();
+	    boolean LDHeatingON = managedBuilding.hasLDHeaeting();
+	    int heatTransportLength = managedBuilding.getHeatTransportLength();
+
+	    Building building = new Building(portUndefined, LDHeatingON, heatTransportLength);
+
+	    ActorTopology buildingHead = new ActorTopology(buildingName);
+	    buildingHead.addActor(buildingName, ActorFactory.createDevice(building));
+	    for (Device device : managedBuilding.getDevices()) {
+		buildingHead.addActorAsChild(buildingName + "/" + device.getClass().getName(),
+			ActorFactory.createDevice(device));
+	    }
+
+	    top.addSubTopology(name, buildingHead);
 	}
-
-	public void attach(BuildingController buildingController) {
-		managedBuildings.add(buildingController);
-	}
-	
-	public void startSimulation() {
-		createTopology();
-		ActorSystem actorSystem = SimulationStarter.initialiseActorSystem(this.top);
-		SimulationStarter.startSimulation(actorSystem, 0, ThesisTopologySimple.NR_OF_ITERATIONS);
-	}
-
-	public void endSimulation() {
-		SimulationStarter.stopSimulation();
-	}
-	
-	private void createTopology() {
-		// Creating Actor Topology
-		this.top = new ActorTopology(this.name);
-		LinProgBehavior linProg = new LinProgBehavior();
-		top.addActor(this.name, ActorFactory.createDevice(linProg));
-
-		for (BuildingController managedBuilding : managedBuildings) {
-			String buildingName = managedBuilding.getName();
-			boolean LDHeatingON = managedBuilding.hasLDHeaeting();
-			int heatTransportLength = managedBuilding.getHeatTransportLength();
-
-			Building building = new Building(portUndefined, LDHeatingON, heatTransportLength);
-
-			ActorTopology buildingHead = new ActorTopology(buildingName);
-			buildingHead.addActor(buildingName, ActorFactory.createDevice(building));
-			for (Device device : managedBuilding.getDevices()) {
-				buildingHead.addActorAsChild(buildingName + "/" + device.getClass().getName(),
-						ActorFactory.createDevice(device));
-			}
-			top.addSubTopology(name, buildingHead);
-		}
-	}
+    }
 
 }
