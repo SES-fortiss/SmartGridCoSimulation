6969ab8c1bca2ca33ed08b5dbded3cdfc0653f05
Saitel Daniela Agudelo Sanabria <agudelo@fortiss.org>
2019-09-04 12:20:30 +0200

    Add methods for directory management in server. Fix #14

---
+++ projects/memapServer/src/main/java/linprogMPC/helper/DirectoryManager.java
@@ -0,0 +1,48 @@
+package linprogMPC.helper;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import linprogMPC.messages.OptimizationResultMessage;
+
+public abstract class DirectoryManager {
+
+	/**
+	 * Add a sub-directory to the directory tree map
+	 * 
+	 * @param folderName Name of new sub-directory
+	 * @param parent     Directory where sub-directory should be created
+	 */
+	private static TreeMap<String, Object> addSubDirectory(String folderName, TreeMap<String, Object> parent) {
+
+		TreeMap<String, Object> newFolder = new TreeMap<String, Object>();
+		parent.put(folderName, newFolder);
+
+		return parent;
+	}
+
+	/**
+	 * Creates a directory hierarchy for simulation results
+	 * 
+	 * @param folderName Name of new sub-directory
+	 * @param parent     Directory where sub-directory should be created
+	 */
+	public static void createDirectoryHierarchy(ArrayList<String> nameCategories, OptimizationResultMessage optResult) {
+
+		for (int i = 0; i < nameCategories.size(); i++) {
+
+			TreeMap<String, Object> buildingFolder = new TreeMap<String, Object>();
+			addSubDirectory("ClientStorageCharge", buildingFolder);
+			addSubDirectory("ClientStorageDischarge", buildingFolder);
+			addSubDirectory("ClientCoupler", buildingFolder);
+			addSubDirectory("ClientVolatileProducer", buildingFolder);
+			addSubDirectory("CSVStorageCharge", buildingFolder);
+			addSubDirectory("CSVStorageDischarge", buildingFolder);
+			addSubDirectory("CSVCoupler", buildingFolder);
+			addSubDirectory("CSVVolatileProducer", buildingFolder);
+
+			optResult.buildingResultsMap.put(nameCategories.get(i), buildingFolder);
+		}
+	}
+
+}
