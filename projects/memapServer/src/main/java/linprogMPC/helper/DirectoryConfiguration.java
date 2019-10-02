9a716ed7fc9709356eb76411ac479d49d4bcf31e
Saitel Daniela Agudelo Sanabria <agudelo@fortiss.org>
2019-10-02 10:24:51 +0200

    Add missing class to task #14.

---
+++ projects/memapServer/src/main/java/linprogMPC/helper/DirectoryConfiguration.java
@@ -0,0 +1,13 @@
+package linprogMPC.helper;
+
+public abstract class DirectoryConfiguration {
+
+	/** Main output directory path */
+	public static String mainDir = "output";
+
+	/** Configuration directory path */
+	public static String resultDir = "results";
+
+	/** Configuration directory path */
+	public static String configDir = "opcconfig";
+}
