19990f72f8ab0a74f376f97a2253d75f628f05f0
bytschkow <bytschkow@fortiss.org>
2020-06-30 22:05:02 +0200

    a new csv reader to read the new format defined by the scenarios

---
+++ projects/memapGui/src/main/java/fortiss/datastructures/TimeDataPoint.java
@@ -0,0 +1,29 @@
+package fortiss.datastructures;
+
+import java.time.LocalDateTime;
+
+/**
+ * 
+ * This class represents one single data value (double) at some certain time (LocalDateTime)
+ * 
+ * @author bytschkow
+ *
+ */
+public class TimeDataPoint {
+	
+	private LocalDateTime dateTime = null;
+	private double value = 0;
+	
+	public TimeDataPoint(LocalDateTime ldt, double value) {
+		this.dateTime = ldt;
+		this.value = value;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	
+	public double getValue() {
+		return value;
+	}
+}
