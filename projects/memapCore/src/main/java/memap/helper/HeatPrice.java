2d8bb5b869adfcf3226c4ed2676afe566b670bf1
denisbyt <bytschkow@fortiss.org>
2021-04-09 16:24:34 +0200

    The labels for heat are the same as for electricity

--- projects/memapCore/src/main/java/memap/helper/HeatPrice.java
+++ projects/memapCore/src/main/java/memap/helper/HeatPrice.java
@@ -3,8 +3,8 @@ package memap.helper;
 public class HeatPrice extends Price {
 
 	private static final String DEFAULT_FILE = "HEATPRICEEXAMPLE";
-	static final String [] labels = { "Price [EUR/kg CO2]" };
-	private static final String units = "[EUR/kg CO2]";
+	private static final String [] labels = { "Price [EUR/kWh]" };
+	private static final String units = "[EUR/kWh]";
 
 	public HeatPrice(double price, int mpcHorizon) {
 		super(price, mpcHorizon);
