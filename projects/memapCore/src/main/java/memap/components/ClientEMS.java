a04957d4841412bd05161cbf23f20797b33f18c5
Mayer <mayer@fortiss.org>
2021-05-04 15:05:36 +0200

    change ClientEMS to extend Connection

--- projects/memapCore/src/main/java/memap/components/ClientEMS.java
+++ projects/memapCore/src/main/java/memap/components/ClientEMS.java
@@ -6,9 +6,9 @@ import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
 
-import akka.basicMessages.AnswerContent;
-import memap.components.prototypes.Device;
+import memap.components.prototypes.Connection;
 import memap.controller.TopologyController;
+import memap.helper.configurationOptions.Optimizer;
 import memap.helperOPCua.BasicClient;
 import memap.messages.extension.NetworkType;
 import memap.messages.planning.ConnectionMessage;
@@ -21,7 +21,7 @@ import memap.messages.planning.ConnectionMessage;
  * @param port
  */
 
-public class ClientEMS extends Device {
+public class ClientEMS extends Connection {
 	
 	/** Reference to the topology */
 	protected TopologyController topologyController;
@@ -31,34 +31,37 @@ public class ClientEMS extends Device {
 	public BasicClient client;
 	public NodeId triggerId;
 	public NodeId connEffId;
-	public double connEff;
 	public double trigger;
 	
-	public ClientEMS(BasicClient client, String name, NodeId triggerId, NodeId connEffId, int port) throws InterruptedException, ExecutionException {
-		super(name, port);
+	public ClientEMS(BasicClient client, TopologyController topologyController, String name, NodeId triggerId, NodeId connEffId, int port) throws InterruptedException, ExecutionException {
+		// How to get the Stings names of connected buildings? From connection Matrix:
+		super("CoSES_H1", "CoSES_H2", 50, client.readFinalDoubleValue(connEffId), 999);
 		this.client = client;
-		this.triggerId = triggerId;
-		this.connEff = client.readFinalDoubleValue(connEffId);
+		this.topologyController = topologyController;
+		this.triggerId = triggerId; 
 	}
 
 	@Override
 	public void makeDecision() {
-//		if (topologyController.getOptimizer() == Optimizer.MILPwithConnections) {
-		connectionMessage.id = fullActorPath;
-		connectionMessage.name = actorName;
-//		connectionMessage.efficiency = connEff;
-		connectionMessage.efficiency = 0.0001;
-		// How to get the Stings names of connected buildings? From connection Matrix
-//		connectionMessage.connectedBuildingFrom = "CoSES_H1";
-//		connectionMessage.connectedBuildingTo = "unknown";
-		connectionMessage.pipeLengthInMeter = 50;
-		connectionMessage.operationalCostEUR = 0.0001;
-		connectionMessage.operationalCostCO2 = 0.0001;
-		connectionMessage.networkType = NetworkType.HEAT;
-//		}
+		if (topologyController.getOptimizer() == Optimizer.MILPwithConnections) {
+			
+			connectionMessage.networkType = NetworkType.HEAT;
+			connectionMessage.name = actorName;
+			connectionMessage.id = fullActorPath;
+		
+			connectionMessage.connectedBuildingFrom = fullActorPath.split("/")[this.fullActorPath.split("/").length-2];		
+			connectionMessage.connectedBuildingTo = connectedBuilding;
+			
+			connectionMessage.efficiency = efficiency;
+			connectionMessage.maxPower = q_max;
+			connectionMessage.pipeLengthInMeter = pipeLengthInMeter;
+			connectionMessage.operationalCostEUR = 0.0001;
+			connectionMessage.operationalCostCO2 = 0.0001;
+		
+		}
 	}
 
-
+	@Override
 	public void handleRequest() {
 		
 		if (triggerId != null) {
@@ -77,14 +80,6 @@ public class ClientEMS extends Device {
 			System.out.println("Trigger written: " + trigger);
 
 		}
-	}
-
-
-
-	@Override
-	public AnswerContent returnAnswerContentToSend() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
